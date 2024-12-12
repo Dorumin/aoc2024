5ea4df0be561aafbbce8f3cadf7c5d4854ec0b4e
Doru <dorminsama@gmail.com>
2024-12-12 06:26:04 -0300

    Flat is justice

--- days/day11/src/lib.rs
+++ days/day11/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt::Write;
+use std::{collections::HashMap, fmt::Write};
 
 use cached::proc_macro::cached;
 
@@ -41,13 +41,20 @@ impl Stones {
         self.stones = stones;
     }
 
-    fn tick_memo(&self, steps: u32) -> usize {
+    #[allow(unused)]
+    fn tick_memo(&self, steps: u32) -> u64 {
         let mut buf = String::new();
 
         self.stones
             .iter()
             .map(|stone| tick_memo(stone.0, steps, &mut buf))
             .sum()
+    }
+
+    fn tick_nore(&self, steps: u32) -> u64 {
+        let stones: Vec<_> = self.stones.iter().map(|stone| stone.0).collect();
+
+        tick_memo_norecurse_goawaylemon(steps, &stones)
     }
 }
 
@@ -71,8 +78,72 @@ impl Stone {
     }
 }
 
-#[cached(key = "(u64, u32)", convert = r##"{ (stone, steps) }"##)]
-fn tick_memo(stone: u64, steps: u32, buf: &mut String) -> usize {
+fn tick_memo_norecurse_goawaylemon(steps: u32, stones: &[u64]) -> u64 {
+    let mut buf = String::new();
+    let mut cache = HashMap::new();
+    let mut stack = Vec::new();
+    let mut total = 0;
+
+    for stone in stones {
+        stack.push((*stone, steps, 0, 0));
+
+        while let Some((stone, steps, current, backref)) = stack.pop() {
+            let stone_index = stack.len();
+
+            if steps == 0 {
+                stack[backref].2 += 1;
+
+                continue;
+            }
+
+            if let Some(sum) = cache.get(&(stone, steps)) {
+                stack[backref].2 += sum;
+
+                continue;
+            }
+
+            if current != 0 {
+                cache.insert((stone, steps), current);
+
+                if stone_index == 0 {
+                    total += current;
+                } else {
+                    stack[backref].2 += current;
+                }
+
+                continue;
+            }
+
+            // unpop
+            stack.push((stone, steps, current, backref));
+
+            write!(buf, "{}", stone).unwrap();
+
+            if buf.len() % 2 == 0 {
+                let left = buf[0..(buf.len() / 2)].parse().unwrap();
+                let right = buf[(buf.len() / 2)..buf.len()].parse().unwrap();
+
+                buf.clear();
+
+                stack.push((right, steps - 1, 0, stone_index));
+                stack.push((left, steps - 1, 0, stone_index));
+            } else if stone == 0 {
+                buf.clear();
+
+                stack.push((1, steps - 1, 0, stone_index));
+            } else {
+                buf.clear();
+
+                stack.push((stone * 2024, steps - 1, 0, stone_index));
+            }
+        }
+    }
+
+    total
+}
+
+#[cached(key = "(u64, u32)", convert = "{ (stone, steps) }")]
+fn tick_memo(stone: u64, steps: u32, buf: &mut String) -> u64 {
     if steps == 0 {
         return 1;
     }
@@ -108,5 +179,5 @@ pub fn part1() {
 pub fn part2() {
     let stones = Stones::from_str(INPUT);
 
-    dbg!(stones.tick_memo(75));
+    dbg!(stones.tick_nore(75));
 }
