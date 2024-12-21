6c2c67f19e2e53bf35317f6a52f2a219bf3f2231
Doru <dorminsama@gmail.com>
2024-12-21 00:09:28 -0300

    no sprawl or scan, index by distance clamp

--- days/day20/src/lib.rs
+++ days/day20/src/lib.rs
@@ -53,23 +53,28 @@ impl Code {
         self.tiles[y * self.width + x]
     }
 
-    fn adj(&self, (x, y): Coord) -> impl Iterator<Item = Coord> {
-        let width = self.width as isize;
-        let height = self.height as isize;
+    fn sprawling(&self, (x, y): Coord, max_dist: usize) -> impl Iterator<Item = (usize, Coord)> {
+        let d = max_dist as isize;
+        let w = self.width as isize;
+        let h = self.height as isize;
 
-        [(0, 1), (1, 0), (0, -1), (-1, 0)]
-            .into_iter()
-            .filter_map(move |(delta_x, delta_y)| {
-                let next = ((x as isize) + delta_x, (y as isize) + delta_y);
+        // We map our diamond indexes horizontally from negative to positive inc. range...
+        (-d..=d).flat_map(move |dx| {
+            // ... and we map our vertical diamond indexes, subtracting the absolute value
+            // of our horizontal index (so we get a by-design manhattan limit)
+            ((-d + dx.abs())..(d - dx.abs() + 1)).filter_map(move |dy| {
+                let next = ((x as isize) + dx, (y as isize) + dy);
 
-                if next.0 < 0 || next.0 >= width || next.1 < 0 || next.1 >= height {
+                if next.0 < 0 || next.0 >= w || next.1 < 0 || next.1 >= h {
                     None
                 } else {
                     let coord = (next.0 as usize, next.1 as usize);
+                    let distance = dx.abs() + dy.abs();
 
-                    Some(coord)
+                    Some((distance as usize, coord))
                 }
             })
+        })
     }
 
     fn index_to_coord(&self, index: usize) -> Coord {
@@ -104,17 +109,23 @@ impl Code {
 
         while current != end {
             let next = self
-                .adj(current)
-                .find(|next| !matches!(self.index(*next), Tile::Wall) && *next != previous)
+                .sprawling(current, 1)
+                .find(|(_, next)| {
+                    !matches!(self.index(*next), Tile::Wall)
+                        && *next != previous
+                        && *next != current
+                })
                 .unwrap();
 
             coords.push(current);
             previous = current;
-            current = next;
+            current = next.1;
         }
 
         coords.push(current);
 
+        // only verify all free tiles are in path in debug builds
+        #[cfg(debug_assertions)]
         for (index, tile) in self.tiles.iter().enumerate() {
             if !matches!(tile, Tile::Wall) {
                 let coord = self.index_to_coord(index);
@@ -126,27 +137,7 @@ impl Code {
         coords
     }
 
-    fn hacks_for<'a>(&'a self, path: &'a [Coord]) -> impl Iterator<Item = usize> + 'a {
-        path.iter().enumerate().flat_map(move |(index, coord)| {
-            let adjacents =
-                self.adj(*coord)
-                    .flat_map(|adj| self.adj(adj))
-                    .filter_map(move |skipped| {
-                        path.iter().enumerate().skip(index).find_map(|(cont, c)| {
-                            if skipped == *c {
-                                Some(index + 2 + (path.len() - cont))
-                            } else {
-                                None
-                            }
-                        })
-                    });
-
-            adjacents
-        })
-    }
-
-    // It's not always DP
-    fn op_hacks(&self) -> u64 {
+    fn count_worthwhile_cheats(&self, min_savings: usize, max_distance: usize) -> i32 {
         let path = self.path_lmao();
 
         // Mapping beats a 9k cell scan
@@ -155,27 +146,14 @@ impl Code {
             indexes[coord.1 * self.width + coord.0] = index;
         }
 
-        // score is len - 1; to beat is at least 100 less than that
+        // score is len - 1; to beat is at least [min_savings] less than that
         // even accounting for this +1 from including start pos, still need to use <=
-        let score_to_beat = path.len() - 100;
+        let score_to_beat = path.len() - min_savings;
 
         let mut hacks_beat = 0;
 
         for (index, coord) in path.iter().enumerate() {
-            // dbg!(index);
-
-            let sprawling = path.iter().skip(index + 100).filter_map(|other| {
-                let man_dist = other.0.abs_diff(coord.0) + other.1.abs_diff(coord.1);
-
-                if man_dist <= 20 {
-                    Some((man_dist, *other))
-                } else {
-                    None
-                }
-            });
-
-            // Remember to skip the 1st which is where we started from
-            for (cheating_score, start) in sprawling {
+            for (cheating_score, start) in self.sprawling(*coord, max_distance) {
                 let end_index = indexes[start.1 * self.width + start.0];
 
                 if end_index != usize::MAX {
@@ -195,23 +173,13 @@ impl Code {
 pub fn part1() {
     let code = Code::from_str(INPUT);
 
-    let path = code.path_lmao();
-
-    dbg!(path.len());
-
-    // score is len - 1; to beat is at least 100 less than that
-    // even accounting for this +1 from including start pos, still need to use <=
-    let score_to_beat = path.len() - 100;
-
-    let hack_count = code.hacks_for(&path).filter(|score| *score <= score_to_beat).count();
-
-    dbg!(hack_count);
+    dbg!(code.count_worthwhile_cheats(100, 2));
 }
 
 pub fn part2() {
     let code = Code::from_str(INPUT);
 
-    dbg!(code.op_hacks());
+    dbg!(code.count_worthwhile_cheats(100, 20));
 }
 
 #[cfg(test)]
@@ -244,14 +212,8 @@ mod tests {
 
         assert_eq!(path.len(), 85);
 
-        let hack_count = code.hacks_for(&path).filter(|score| *score < 84).count();
+        let cheat_count = code.count_worthwhile_cheats(64, 2);
 
-        dbg!(hack_count);
-
-        let score_to_beat = path.len() - 64;
-
-        let hack_count = code.hacks_for(&path).filter(|score| *score <= score_to_beat).count();
-
-        dbg!(hack_count);
+        assert_eq!(cheat_count, 1);
     }
 }
